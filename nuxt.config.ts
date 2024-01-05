ab0dcbb23c8f1d2836af57b8b057a27776099206
Theodor Tomander Skippervold <theodor.tomander.skippervold@bidbax.no>
2024-01-05 17:54:06 +0100

    TailwindCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,12 @@ export default defineNuxtConfig({
   typescript: {
     typeCheck: true
   },
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  }
 })
